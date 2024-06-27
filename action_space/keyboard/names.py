e9900f9946afcd6c837f1afe41e9362907d1c615
SamthinkGit <sebastianmayorquin@gmail.com>
2024-06-27 21:57:30 +0200

    improved pynput actions

---
+++ action_space/keyboard/names.py
@@ -0,0 +1,113 @@
+class KeyNames:
+
+    CTRL: list[str] = [
+        "ctrl",
+        "control",
+        "ctrlkey",
+        "controlkey",
+        "ctrl_key",
+        "control_key",
+        "key_ctrl",
+        "key_control",
+        "ctrl_pressed",
+        "ctrl_pressed",
+        "control_pressed",
+        "ctrl_modifier",
+        "control_modifier",
+        "ctrl_button",
+        "control_button",
+    ]
+    SHIFT: list[str] = [
+        "shift",
+        "shiftkey",
+        "shift_key",
+        "key_shift",
+        "shift_pressed",
+        "shift_modifier",
+        "shift_button",
+        "shift_flag",
+        "shift_status",
+        "shift_state",
+        "shift_active",
+    ]
+    ALT: list[str] = [
+        "alt",
+        "altkey",
+        "alt_key",
+        "key_alt",
+        "alt_pressed",
+        "is_alt_pressed",
+        "alt_modifier",
+        "alt_button",
+        "alt_flag",
+        "alt_status",
+        "alt_state",
+        "alt_active",
+    ]
+    META: list[str] = [
+        "meta",
+        "metakey",
+        "meta_key",
+        "key_meta",
+        "meta_pressed",
+        "meta_modifier",
+        "meta_button",
+        "meta_trigger",
+    ]
+    ESC: list[str] = [
+        "esc",
+        "escape",
+        "escapekey",
+        "escape_key",
+        "key_escape",
+        "escape_pressed",
+        "escape_button",
+        "escape_trigger",
+    ]
+    TAB: list[str] = [
+        "space",
+        "spacekey",
+        "space_key",
+        "key_space",
+        "space_pressed",
+        "space_button",
+        "space_trigger",
+    ]
+    BACKSPACE: list[str] = [
+        "back",
+        "backkey",
+        "backspace",
+        "backspacekey",
+        "backspace_key",
+        "key_backspace",
+        "backspace_pressed",
+        "backspace_button",
+        "backspace_trigger",
+    ]
+    DELETE: list[str] = [
+        "del",
+        "delete",
+        "deletekey",
+        "delete_key",
+        "del_key",
+        "delpressed",
+        "delete_pressed",
+        "deletepressed",
+    ]
+    HOME: list[str] = [
+        "home",
+        "home_key",
+        "homekey",
+        "homepressed",
+        "is_home",
+        "homestatus",
+        "home_status",
+    ]
+    END: list[str] = [
+        "end",
+        "end_key",
+        "endkey",
+        "endkeypress",
+        "press_end",
+        "endpressed"
+    ]
--- action_space/keyboard/pynput.py
+++ action_space/keyboard/pynput.py
@@ -3,11 +3,19 @@ import time
 from pynput.keyboard import Controller
 from pynput.keyboard import Key
 
+from action_space.keyboard.names import KeyNames
 from ecm.tools.registry import ItemRegistry
 
 keyboard = Controller()
 
 
+@ItemRegistry.alias(
+    [
+        "type",
+        "type_text",
+        "write_text",
+    ]
+)
 @ItemRegistry.register_function
 def write(text: str):
     """Writes the given text. If you are writing a text/sentence or some word/s, should use this function. This write does not include Enter. Usage: write('Hello, world!')"""  # noqa
@@ -16,72 +24,54 @@ def write(text: str):
         time.sleep(0.03)
 
 
+@ItemRegistry.alias(["press_key", "key"])
 @ItemRegistry.register_function
 def press(key: str):
     """Presses the specified key. Usage: press('enter')"""
     key = key.lower()
-    match key:
-        case "enter":
-            keyboard.press(Key.enter)
-            keyboard.release(Key.enter)
-        case "backspace":
-            keyboard.press(Key.backspace)
-            keyboard.release(Key.backspace)
-        case "space":
-            keyboard.press(Key.space)
-            keyboard.release(Key.space)
-        case "tab":
-            keyboard.press(Key.tab)
-            keyboard.release(Key.tab)
-        case "esc":
-            keyboard.press(Key.esc)
-            keyboard.release(Key.esc)
-        case "up":
-            keyboard.press(Key.up)
-            keyboard.release(Key.up)
-        case "down":
-            keyboard.press(Key.down)
-            keyboard.release(Key.down)
-        case "left":
-            keyboard.press(Key.left)
-            keyboard.release(Key.left)
-        case "right":
-            keyboard.press(Key.right)
-            keyboard.release(Key.right)
-        case _:
-            keyboard.press(key)
-            keyboard.release(key)
+    keyboard.tap(eval(f"Key.{key}", globals()))
 
 
 @ItemRegistry.register_function
 def hotkey(*keys: str):
     """Presses a combination of keys. Usage: hotkey('ctrl', 's')"""
+    if isinstance(keys[0], list) or isinstance(keys[0], tuple):
+        keys = keys[0]
+
+    keys_pressed: list[str | Key] = []
     for key in keys:
-        if key == "ctrl":
-            keyboard.press(Key.ctrl)
-        elif key == "shift":
-            keyboard.press(Key.shift)
-        elif key == "alt":
-            keyboard.press(Key.alt)
-        elif key == "cmd" or key == "command":
-            keyboard.press(Key.cmd)
-        else:
-            keyboard.press(key)
+        key = _parse_key(key)
+        keys_pressed.append(key)
+        keyboard.press(key)
+
+    for key in keys_pressed:
+        keyboard.release(key)
 
 
-@ItemRegistry.register_function
-def type(text: str):
-    """Types the given text. Does not include enter. Use me when typing a word or text. Usage: type('Hello, world!')"""
-    write(text)
-
-
-@ItemRegistry.register_function
-def type_text(text: str):
-    """Types the given text. Does not include enter. Use me when typing a word or text. Usage: type_text('Hello, world!')""" # noqa
-    write(text)
-
-
-@ItemRegistry.register_function
-def press_key(text: str):
-    """Presses the specified key. Usage: press_key('enter')"""
-    press(text)
+def _parse_key(key: str) -> str | Key:
+    """Parses a special key into Pynput Key type. If not found returns the same str"""
+    key = key.lower()
+    match key:
+        case _ if key in KeyNames.CTRL:
+            return Key.ctrl
+        case _ if key in KeyNames.SHIFT:
+            return Key.shift
+        case _ if key in KeyNames.ALT:
+            return Key.alt
+        case _ if key in KeyNames.ESC:
+            return Key.esc
+        case _ if key in KeyNames.TAB:
+            return Key.tab
+        case _ if key in KeyNames.HOME:
+            return Key.home
+        case _ if key in KeyNames.END:
+            return Key.end
+        case _ if key in KeyNames.BACKSPACE:
+            return Key.backspace
+        case _ if key in KeyNames.DELETE:
+            return Key.delete
+        case _:
+            try:
+                return eval(f"Key.{key}")
+            except Exception:
+                return key
