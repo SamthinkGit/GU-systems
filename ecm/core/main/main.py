661814da31793c6af4e6850469fbdbab847443f4
SamthinkGit <sebastianmayorquin@gmail.com>
2025-03-12 13:53:31 +0100

    Refactored main

--- ecm/core/main/main.py
+++ ecm/core/main/main.py
@@ -12,7 +12,59 @@ DEFAULT_COGNITION = "fastreact"
 DEFAULT_EXECUTOR = "pyxcel"
 
 
-def main(args):
+def main():
+    # ---------- ACTION SPACE ---------
+    import action_space.keyboard.actions  # noqa
+    import action_space.experimental.screenshot.actions  # noqa
+
+    # ----------------------------------
+    ItemRegistry().load_all()
+
+    argparser = argparse.ArgumentParser(description="Run the ECM project.")
+    argparser.add_argument(
+        "--verbose", action="store_true", help="Enable verbose outputs from the agents"
+    )
+    argparser.add_argument("--debug", action="store_true", help="Enable debug output")
+    argparser.add_argument(
+        "--langchain-debug",
+        action="store_true",
+        help="Enable LangChain Debug information",
+    )
+    argparser.add_argument(
+        "--show-exelent",
+        action="store_true",
+        help="Shows Exelent file obtained from the Cognition Layer",
+    )
+
+    argparser.add_argument(
+        "--host",
+        action="store_true",
+        help="[CAUTION] Executes the exelent generated code into the host machine. Please use in a safe environment.",
+    )
+
+    argparser.add_argument(
+        "--agent",
+        type=str,
+        help="Select the Cognition Layer interface",
+        choices=COGNITION_LAYERS,
+        default=DEFAULT_COGNITION,
+    )
+
+    argparser.add_argument(
+        "--executor",
+        type=str,
+        help="Select the Execution Layer interface",
+        choices=EXECUTION_LAYERS,
+        default=DEFAULT_EXECUTOR,
+    )
+    args = argparser.parse_args()
+
+    if args.debug:
+        ecm.shared.LOG_LEVEL = logging.DEBUG
+    if args.langchain_debug:
+        set_debug(True)
+    if not args.host:
+        ItemRegistry().invalidate(tools=False)
 
     logger = get_logger("main")
 
@@ -67,57 +119,4 @@ def main(args):
 
 
 if __name__ == "__main__":
-    # ---------- ACTION SPACE ---------
-    import action_space.keyboard.actions  # noqa
-    import action_space.experimental.screenshot.actions  # noqa
-
-    # ----------------------------------
-    ItemRegistry().load_all()
-
-    argparser = argparse.ArgumentParser(description="Run the ECM project.")
-    argparser.add_argument(
-        "--verbose", action="store_true", help="Enable verbose outputs from the agents"
-    )
-    argparser.add_argument("--debug", action="store_true", help="Enable debug output")
-    argparser.add_argument(
-        "--langchain-debug",
-        action="store_true",
-        help="Enable LangChain Debug information",
-    )
-    argparser.add_argument(
-        "--show-exelent",
-        action="store_true",
-        help="Shows Exelent file obtained from the Cognition Layer",
-    )
-
-    argparser.add_argument(
-        "--host",
-        action="store_true",
-        help="[CAUTION] Executes the exelent generated code into the host machine. Please use in a safe environment.",
-    )
-
-    argparser.add_argument(
-        "--agent",
-        type=str,
-        help="Select the Cognition Layer interface",
-        choices=COGNITION_LAYERS,
-        default=DEFAULT_COGNITION,
-    )
-
-    argparser.add_argument(
-        "--executor",
-        type=str,
-        help="Select the Execution Layer interface",
-        choices=EXECUTION_LAYERS,
-        default=DEFAULT_EXECUTOR,
-    )
-    args = argparser.parse_args()
-
-    if args.debug:
-        ecm.shared.LOG_LEVEL = logging.DEBUG
-    if args.langchain_debug:
-        set_debug(True)
-    if not args.host:
-        ItemRegistry().invalidate(tools=False)
-
-    main(args)
+    main()
