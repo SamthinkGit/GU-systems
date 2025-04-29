7514421ffb884b14e1b2a04a286f498a64296542
Sebastian Mayorquin <sebastianmayorquin@gmail.com>
2025-04-29 11:16:18 +0200

    Added debug message to installer.bat

--- installer/install.bat
+++ installer/install.bat
@@ -1,3 +1,4 @@
 cd /d "%~dp0"
 python -m pip install streamlit
 streamlit run ./installer/streamlit/app.py || streamlit run ./streamlit/app.py || streamlit run ./app.py
+set /p dato=if you see me, please send this logs to the developer
